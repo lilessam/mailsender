bb202356d90896c700a2c784592d065cb4c85a25
Ahmed Essam <erwinchafros@gmail.com>
2016-06-06 19:57:35 +0200

    Updating sentences for new mail test feature

--- lang/ar/lang.php
+++ lang/ar/lang.php
@@ -6,19 +6,27 @@ return [
     'backend' => [
         'title' => 'مرسل البريد',
         ],
-    'controller' => [
-        'groups' => 'اختر مجموعة',
-        'subject' => 'عنوان الرسالة',
-        'typesubject' => 'اكتب عنوان الرسالة هنا',
-        'message' => 'اكتب الرسالة',
-        'type' => 'اكتب الرسالة هنا',
-        'submit' => 'أرسل الرسالة',
+    'controller'        => [
+        'groups'        => 'اختر مجموعة',
+        'subject'       => 'عنوان الرسالة',
+        'typesubject'   => 'اكتب عنوان الرسالة هنا',
+        'message'       => 'اكتب الرسالة',
+        'type'          => 'اكتب الرسالة هنا',
+        'submit'        => 'أرسل الرسالة',
         ],
-    'sent' => 'تم إرسال الرسالة بنجاح إلى ',
-    'users' => ' عضو',
-    'nousers' => 'لا يوجد أعضاء في هذه المجموعة',
-    'callout' => [
-      'title' => 'Backend users mail sender',
-      'body' => 'This plugin allows you to send a mail to a specific backend group',
+    'sent'      => 'تم إرسال الرسالة بنجاح إلى ',
+    'users'     => ' عضو',
+    'nousers'   => 'لا يوجد أعضاء في هذه المجموعة',
+    'callout'   => [
+        'title' => 'مرسل البريد إلى الأعضاء',
+        'body'  => 'هذه الإضافة تساعدك على إرسال بريد جماعي إلى أعضاء موقعك',
     ],
+    'test' => [
+        'text'              => 'يمكنك إرسال بريد تجريبي عبر هذا النموذج',
+        'email'             => 'أكتب بريدك الإلكتروني هنا',
+        'send_mail'         => 'ارسل رسالة تجريبية',
+        'email_placeholder' => 'email@email.com',
+        'sent'              => 'تم إرسال الرسالة بنجاح',
+        ],
+    'error_nodata' => 'جميع الحقول إجبارية',
   ];
